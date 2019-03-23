5fec9f0aa03040bcb43e78a47b9df8d3fdeeaf6d
2014005187 <2014005187@hanyang.ac.kr>
2019-03-24 06:22:14 +0900

    week 1

--- Jinaria/2231.cpp
+++ Jinaria/2231.cpp
@@ -12,13 +12,15 @@ int main() {
 	while (temp /= 10) {
 		cipher++;
 	}
-    int sub_origin_num = cipher * 10;
+    // every cipher has range 0 ~ 9, so difference between origin_num and N is digit multiplied by 10 at most.
+    int sub_origin_num = cipher * 10; 
     int origin_num = 0;
     for (int i = N - sub_origin_num; i < N; i++) {
         int sum = i;
         for (int j = 0; j < cipher; i++) {
             sum += i / pow(10, j) % 10;
         }
+        // We want the mininmum number, so we stop as soon as we find the matching value.
         if (sum == N) {
             origin_num = i;
             break;
