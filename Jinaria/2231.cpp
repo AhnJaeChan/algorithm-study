dd3e3228a199937f8ce8034938eb36f759b3fce8
2014005187 <2014005187@hanyang.ac.kr>
2019-03-24 06:16:17 +0900

    week 1

--- Jinaria/2231.cpp
+++ Jinaria/2231.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <utility>
 #include <cmath>
 
 using namespace std;
 
-const int INF = 100000000;
-
-int ret = INF;
-
-void check(int cipher, vector<int> &num, int origin_num) {
-	if (cipher == 0) {
-		for (int i = 0; i < num.size(); i++) {
-			int temp = origin_num % (int)pow(10.0, i + 1) / pow(10.0, i);
-			if (num[i] != temp) {
-				return;
-			}
-		}
-		ret = min(ret, origin_num);
-		return;
-	}
-	else {
-		for (int i = 0; i <= 9; i++) {
-			num.push_back(i);
-			check(cipher - 1, num, origin_num - i);
-			num.pop_back();
-		}
-	}
-}
 
 int main() {
 	int N;
@@ -37,8 +12,17 @@ int main() {
 	while (temp /= 10) {
 		cipher++;
 	}
-	vector<int> num;
-	check(cipher, num, N);
-	if (ret == INF) cout << 0 << endl;
-	else cout << ret << endl;
+    int sub_origin_num = cipher * 10;
+    int origin_num = 0;
+    for (int i = N - sub_origin_num; i < N; i++) {
+        int sum = i;
+        for (int j = 0; j < cipher; i++) {
+            sum += i / pow(10, j) % 10;
+        }
+        if (sum == N) {
+            origin_num = i;
+            break;
+        }
+
+    }
 }
