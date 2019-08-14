0db0c5c1075ae2ea08bb92289cf17205ade01481
Ahn Jaechan <jaechan5256@gmail.com>
2019-08-14 13:38:42 +0900

    baekjoon/9008 implemented

--- AhnJaeChan/baekjoon/9008/main.cpp
+++ AhnJaeChan/baekjoon/9008/main.cpp
@@ -1,53 +1,95 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+//#define DEBUG_MODE
 
 using namespace std;
 
-int T, N;
-
-vector<pair<int, int>> vpoints;
-vector<pair<int, int>> hpoints;
-
 class Point {
+public:
   int key;
-  int x, y;
+  int p1, p2;
 
-  bool operator<(Point const &comp) {
-    return x < comp.x;
+  Point(int key, int p1, int p2) {
+    this->key = key;
+    this->p1 = p1;
+    this->p2 = p2;
+  }
+
+  bool operator<(const Point &comp) const {
+    // sort by p1, if p1 is same -> sort by p2 (ascending)
+    return (p1 != comp.p1) ? (p1 < comp.p1) : (p2 < comp.p2);
+  }
+
+  friend ostream &operator<<(std::ostream &os, const Point &point) {
+    return os << point.p1 << ", " << point.p2 << "(" << point.key << ")";
   }
 };
+
+const int MAX_POINTS = 10000;
+
+int T, N;
+vector<Point> vpoints;
+vector<Point> hpoints;
+pair<int, int> connection[MAX_POINTS];
 
 bool solve() {
   // Check each verticals
   for (int i = 0; i < N; i += 2) {
-    int x1 = vpoints[i].first;
-    int y1 = vpoints[i].second;
-    int y2 = vpoints[i + 1].second;
+    int x1 = vpoints[i].p1;
+    int y1 = vpoints[i].p2;
+    int y2 = vpoints[i + 1].p2;
 
     // Check for horizontal intersection
     for (int j = 0; j < N; j += 2) {
-      if (hpoints[j].first > y1 && hpoints[j].first < y2) {
-        if (hpoints[j].second < x1 && hpoints[j + 1].second > x1) {
+      if (hpoints[j].p1 > y1 && hpoints[j].p1 < y2) {
+        if (hpoints[j].p2 < x1 && hpoints[j + 1].p2 > x1) {
           return false;
         }
       }
     }
   }
 
-  for (int i = 0; i < N - 2; i += 2) {
-    if (vpoints[i + 1].second == vpoints[i + 2].second || hpoints[i + 1].second == hpoints[i + 2].second) {
-      return false;
-    }
+  // index = key
+  // first = vertical connection index
+  // second = horizontal connection index
+  for (int i = 0; i < N; i += 2) {
+    connection[vpoints[i].key].first = vpoints[i + 1].key;
+    connection[vpoints[i + 1].key].first = vpoints[i].key;
+
+    connection[hpoints[i].key].second = hpoints[i + 1].key;
+    connection[hpoints[i + 1].key].second = hpoints[i].key;
   }
 
-  int cnt = 0;
+#ifdef DEBUG_MODE
+  cout << endl;
+  for (int i = 0; i < vpoints.size(); ++i) {
+    cout << i << ": " << connection[i].first << ", " << connection[i].second << endl;
+  }
+#endif
 
-  auto start = vpoints[0];
-  while (cnt != vpoints.size()) {
-    cnt++;
+  // Start from key 0's vertical connection and follow the path
+  // until it comes back to index 0. If the count is the same
+  // as the number of points, it means that the line connection
+  // has traversed around all the points. If not, there are
+  // at least 2 independent polygons.
+  int count = 1;
+  int key = connection[0].first;
+
+  while (key != 0) {
+#ifdef DEBUG_MODE
+    cout << key << " -> ";
+#endif
+    key = (count % 2 == 0) ? connection[key].first : connection[key].second;
+    count++;
   }
 
-  return true;
+#ifdef DEBUG_MODE
+  cout << "0" << endl << count << endl;
+#endif
+
+  return count == vpoints.size();
 }
 
 int main() {
@@ -67,20 +109,12 @@ int main() {
     int x, y;
     for (int i = 0; i < N; ++i) {
       cin >> x >> y;
-      vpoints.emplace_back(make_pair(x, y));
-      hpoints.emplace_back(make_pair(y, x));
+      vpoints.emplace_back(Point(i, x, y));
+      hpoints.emplace_back(Point(i, y, x));
     }
 
     sort(vpoints.begin(), vpoints.end());
     sort(hpoints.begin(), hpoints.end());
-
-    for (auto p: vpoints) {
-      cout << p.first << ", " << p.second << endl;
-    }
-
-    for (auto p: hpoints) {
-      cout << p.first << ", " << p.second << endl;
-    }
 
     cout << (solve() ? "YES" : "NO") << endl;
   }
