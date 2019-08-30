0d220d50b81e418fe7abd2f4d48b0d25d7cb6507
Ahn Jaechan <jaechan5256@gmail.com>
2019-08-30 16:58:21 +0900

    baekjoon 10254 - Attempt 3 convex hull algorithm applied, time limit extended.

--- AhnJaeChan/baekjoon/10254.cpp
+++ AhnJaeChan/baekjoon/10254.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <cmath>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,10 +22,6 @@ public:
     return sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
   }
 
-  bool is_inside(const Point &center, double radius) {
-    return euclidean_distance(center, *this) <= radius;
-  }
-
   friend ostream &operator<<(ostream &os, const Point &p);
   friend istream &operator>>(istream &is, Point &p);
 };
@@ -38,6 +36,107 @@ istream &operator>>(istream &is, Point &p) {
   return is;
 }
 
+Point p0; // For sorting;
+
+int orientation(Point p, Point q, Point r) {
+  int val = (q.y - p.y) * (r.x - q.x) -
+            (q.x - p.x) * (r.y - q.y);
+
+  if (val == 0) return 0;  // colinear
+  return (val > 0) ? 1 : 2; // clock or counterclock wise
+}
+
+int compare(const void *vp1, const void *vp2) {
+  auto *p1 = (Point *) vp1;
+  auto *p2 = (Point *) vp2;
+
+  int o = orientation(p0, *p1, *p2);
+  if (o == 0) {
+    return (Point::euclidean_distance(p0, *p2) >= Point::euclidean_distance(p0, *p1)) ? -1 : 1;
+  }
+
+  return (o == 2) ? -1 : 1;
+}
+
+Point next_to_top(stack<Point> &S) {
+  Point p = S.top();
+  S.pop();
+  Point res = S.top();
+  S.push(p);
+  return res;
+}
+
+void convex_hull(Point points[], int n) {
+  int min_idx = 0;
+
+  for (int i = 1; i < n; i++) {
+    if (points[i].y < points[min_idx].y) {
+      min_idx = i;
+    } else if (points[i].y == points[min_idx].y) {
+      if (points[i].x < points[min_idx].x) {
+        min_idx = i;
+      }
+    }
+  }
+
+  swap(points[0], points[min_idx]);
+  p0 = points[0]; // Global variable, must be called before sorting since it is used inside the compare function
+
+  qsort(&points[1], n - 1, sizeof(Point), compare);
+
+  int m = 1;
+  for (int i = 1; i < n; i++) {
+    while (i < n - 1 && orientation(p0, points[i], points[i + 1]) == 0) {
+      i++;
+    }
+    points[m] = points[i];
+    m++;
+  }
+
+  if (m < 3) {
+    // Can't execute convex hull
+    return;
+  }
+
+  stack<Point> S;
+  S.push(points[0]);
+  S.push(points[1]);
+  S.push(points[2]);
+
+  for (int i = 3; i < m; i++) {
+    while (orientation(next_to_top(S), S.top(), points[i]) != 2) {
+      S.pop();
+    }
+    S.push(points[i]);
+  }
+
+  Point outer_points[S.size()];
+
+  m = 0;
+  while (!S.empty()) {
+    outer_points[m++] = S.top();
+    S.pop();
+  }
+
+  // Find max distance
+  double max_distance = 0;
+  pair<Point, Point> end_points;
+
+  for (int i = 0; i < m; i++) {
+    Point p = outer_points[i];
+    for (int j = i + 1; j < m; j++) {
+      double distance = Point::euclidean_distance(p, outer_points[j]);
+      if (max_distance < distance) {
+        end_points.first = p;
+        end_points.second = outer_points[j];
+        max_distance = distance;
+      }
+    }
+  }
+
+  cout << end_points.first << end_points.second << endl;
+}
+
 int main(int argc, char *argv[]) {
   int T;
 
@@ -48,27 +147,12 @@ int main(int argc, char *argv[]) {
 
     cin >> n;
 
-    Point ps, pt;
-    cin >> ps >> pt;
-
-    double radius = Point::euclidean_distance(ps, pt);
-
-    Point p;
-    for (int i = 2; i < n; i++) {
-      cin >> p;
-
-      if (!(p.is_inside(ps, radius) && p.is_inside(pt, radius))) {
-        if (Point::euclidean_distance(p, ps) > Point::euclidean_distance(p, pt)) {
-          pt = p;
-        } else {
-          ps = p;
-        }
-
-        radius = Point::euclidean_distance(ps, pt);
-      }
+    Point points[n];
+    for (int i = 0; i < n; i++) {
+      cin >> points[i];
     }
 
-    cout << ps << pt << endl;
+    convex_hull(points, n);
   }
 
   return 0;
