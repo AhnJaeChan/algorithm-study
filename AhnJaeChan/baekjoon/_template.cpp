d984e30326abd122233141a60baa62ff83966b07
Ahn Jaechan <jaechan5256@gmail.com>
2019-09-09 14:53:42 +0900

    template file for baekjoon problems

---
+++ AhnJaeChan/baekjoon/_template.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 안재찬 on 09/09/2019.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int T;
+  cin >> T;
+
+  for (int test_case = 0; test_case < T; test_case++) {
+  }
+
+  return 0;
+}
