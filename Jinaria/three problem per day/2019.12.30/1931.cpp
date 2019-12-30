c7deef3d5bbbc07d481e1fbac90101062edec78f
2014005187 <2014005187@hanyang.ac.kr>
2019-12-30 22:00:34 +0900

    1931

---
+++ Jinaria/three problem per day/2019.12.30/1931.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    vector<pair<int, int>> v(N);
+    for(int i = 0; i < N; ++i) {
+        scanf("%d %d", &v[i].first, &v[i].second);
+    }  
+
+    
+}
