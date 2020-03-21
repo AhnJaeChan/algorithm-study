6b33a8b9241dcca89411388e67c87b1d7e37ea71
진승기 <wlstmdrl0925@gmail.com>
2020-03-21 16:39:59 +0900

    Solve Three problem

--- Jinaria/three problem per day/2020.3.20/14889.cpp
+++ Jinaria/three problem per day/2020.3.20/14889.cpp
@@ -7,31 +7,48 @@ using namespace std;
 
 #define MAX_NUM     1234567890
 
-int stat[21][21];
+int state[21][21];
 
 int N;
 
-int func(int level, int startpick, int start, int link) {
+int func(int level, int startpick, int start, int mask) {
     if (level == N / 2) {
-
+        int tlink = 0;
+        for (int i = 0; i < N; ++i) {
+            if ((~mask) & (1 << i)) {
+                for (int j = i + 1; j < N; ++j) {
+                    if ((~mask) & (1 << j)) {
+                        tlink += (state[i][j] + state[j][i]);
+                    }
+                }
+            }
+        }
+        return abs(tlink - start);
     }
 
     int ret = MAX_NUM;
-    for (int i = 0; i < N / 2 - level; ++i) {
-        ret = min(ret, func(level + 1, ))
+    for (int i = startpick + 1; i < startpick + 1 + N / 2 && i < N; ++i) {
+        int tstart = start;
+        for (int j = 0; j < N; ++j) {
+            if (j != i && (1 << j) & mask) {
+                tstart += (state[i][j] + state[j][i]);
+            }
+        }
+        ret = min(ret, func(level + 1, i, tstart, (mask | (1 << i))));
     }
+    return ret;
 }
 
 int main() {
     ios::sync_with_stdio(false);
-    
+
     cin >> N;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            cin >> stat[i][j];
+            cin >> state[i][j];
         }
     }
 
-    cout << func(0, 0, 0, 0) << '\n';
+    cout << func(0, -1, 0, 0) << '\n';
 
 }
